5b88b37de04edf9f5f257fe3ca6f9f8b5ee2cdaf
yullidias <yulli.dias@hotmail.com>
2020-02-22 19:14:54 -0300

    source to sum, calculate factorial and fibonacci

---
+++ src/factorial.hs
@@ -0,0 +1,3 @@
+factorial 0 = 1
+factorial 1 = 1
+factorial n = factorial(n-1) * n
---
+++ src/sum.hs
@@ -0,0 +1,5 @@
+{-somar os numeros de 1 até n
+Ex: n=4, soma = 1 + 2 + 3 + 4 = 10-}
+
+soma 1 = 1
+soma n = soma(n-1) + n
