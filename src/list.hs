a349aac7f698ed4c1c42a7600d77a8fc1c9902a7
yullidias <yulli.dias@hotmail.com>
2020-04-02 08:13:24 -0300

    Add reverse, verifiy if element is in list, greater element, validate if all elemets is pair

--- src/list.hs
+++ src/list.hs
@@ -1,5 +1,5 @@
 -- [] -> empty list
--- : -> add element on top of list
+-- : -> element on top of list
 
 size_list [] = 0
 size_list (head_element : tail_list) = 1 + size_list tail_list
@@ -10,3 +10,23 @@ equal [] _ = False
 equal _ [] = False
 equal (head_a: tail_a) (head_b: tail_b) | (head_a == head_b) = equal tail_a tail_b
                                         | otherwise = False
+
+my_reverse :: [t] -> [t]
+my_reverse [] = []
+my_reverse (head:tail) = reverse tail ++ [head]
+
+is_in :: Int -> [Int] -> Bool
+is_in _ [] = False
+is_in x (head:tail) | x == head = True
+                    | otherwise = is_in x tail
+
+greater :: [Int] -> Int
+greater [x] = x
+greater (head:tail) | (head > greater tail) = head
+                    | otherwise = greater tail
+
+
+pair :: [Int] -> Bool
+pair [] = True
+pair (head:tail) | (mod head 2 /= 0) = False -- /= -> difference
+                 | otherwise = pair tail
