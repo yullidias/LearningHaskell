41ef5f70f695e7194758adc1155608511b2e98dc
yullidias <yulli.dias@hotmail.com>
2020-04-09 08:06:40 -0300

    - examples if-else, case and guard - exemples print, putStr and read

--- src/guard.hs
+++ src/guard.hs
@@ -3,3 +3,14 @@
 gard x | (x == 0) = 0
        | (x == 1) = 1
        | otherwise = 10
+
+if_pair :: Int -> Bool
+if_pair n = if (mod n 2 == 0) then True else False
+
+case_pair :: Int -> Bool
+case_pair n =   case (mod n 2 == 0) of
+              True -> True
+              False -> False
+
+guard_pair n | (mod n 2 == 0) = True
+             | otherwise = False
--- src/sum.hs
+++ src/sum.hs
@@ -3,3 +3,7 @@ Ex: n=4, soma = 1 + 2 + 3 + 4 = 10-}
 
 soma 1 = 1
 soma n = soma(n-1) + n
+
+show "print on screen"
+putStr "print on screen"
+read "10" + 10 {- le conteudo e transforma para inteiro -}
